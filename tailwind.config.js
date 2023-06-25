a71dd03ceefbe08d6a3ad19aedfe55a5dd193f2f
AnnatarHe <annatar.he+github@gmail.com>
2023-06-25 20:59:00 +0800

    feat(style): add style

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,24 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
+  daisyui: {
+    prefix: 'daisyui',
+    themes: [
+      {
+        pp: {
+          "primary": "#2dd4bf",
+          "secondary": "#60a5fa",
+          "accent": "#d946ef",
+          "neutral": "#2a323c",
+          "base-100": "#1d232a",
+          "info": "#3abff8",
+          "success": "#5eead4",
+          "warning": "#fbbd23",
+          "error": "#f87272",
+        },
+      },
+    ],
+  },
   theme: {
     extend: {},
   },
