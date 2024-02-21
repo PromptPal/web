becc289e9e77b73ad9a635b4c751024540740248
AnnatarHe <annatar.he+github@gmail.com>
2024-02-22 00:35:46 +0800

    chore(header): update logo

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -9,8 +9,8 @@ function Header() {
   return (
     <header className='py-2 px-6 lg:px-0 backdrop-blur-sm mt-4 rounded-full items-center justify-between flex z-50'>
       <div className='flex items-center'>
-        <Link to='/' className=' mr-2 px-2 py-1 dark:text-white rounded transition-colors duration-300'>
-          <h2>PromptPal</h2>
+        <Link to='/' className='mr-2 px-2 py-1 rounded font-bold text-transparent bg-clip-text bg-gradient-to-tr from-blue-300 to-purple-700'>
+          PromptPal
         </Link>
         <div className='text-xs daisyui-badge daisyui-badge-info'>Alpha</div>
         <ProjectLiteInfo />
