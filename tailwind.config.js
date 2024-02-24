aa59ee79c929fea80e700b0937e4afcb515ddae5
AnnatarHe <annatar.he+github@gmail.com>
2024-02-24 17:04:45 +0800

    fix(css): update tailwindcss configuration to respect color scheme set by mantine

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
   ],
+  darkMode: ['class', '[data-mantine-color-scheme="dark"]'],
   daisyui: {
     prefix: 'daisyui-',
     themes: [
       {
         pp: {
-          "primary": "#2dd4bf",
-          "secondary": "#60a5fa",
-          "accent": "#d946ef",
-          "neutral": "#2a323c",
-          "base-100": "#1d232a",
-          "info": "#3abff8",
-          "success": "#5eead4",
-          "warning": "#fbbd23",
-          "error": "#f87272",
+          'primary': '#2dd4bf',
+          'secondary': '#60a5fa',
+          'accent': '#d946ef',
+          'neutral': '#2a323c',
+          'base-100': '#1d232a',
+          'info': '#3abff8',
+          'success': '#5eead4',
+          'warning': '#fbbd23',
+          'error': '#f87272',
         },
       },
     ],
@@ -26,6 +27,6 @@ export default {
     extend: {},
   },
   // eslint-disable-next-line no-undef
-  plugins: [require("daisyui")],
+  plugins: [require('daisyui')],
 }
 
